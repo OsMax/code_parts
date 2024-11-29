546f4bcb05a10f7a0949cb17d38b8020e1be0229
OsMax <www.osmax@gmail.com>
2024-11-29 14:05:22 +0200

    provider store to app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-// import logo from "./logo.svg";
+import { Provider } from "react-redux";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
+import store from "./redux/store";
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Header />
       <Main />
-    </>
+    </Provider>
   );
 }
 
