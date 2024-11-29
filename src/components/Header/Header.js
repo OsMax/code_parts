178aa0e5093c6fdfb54406d947ca4dab14f4a197
OsMax <www.osmax@gmail.com>
2024-11-29 14:05:50 +0200

    select to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,32 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setChoice } from "../../redux/store";
+import css from "./Header.module.css";
+
 const Header = () => {
+  const dispatch = useDispatch();
+  const selectedChoice = useSelector((state) => state.choice.selectedChoice);
+
+  // const [choice, setCoice] = useState("");
+
+  const handleChange = (event) => {
+    dispatch(setChoice(event.target.value));
+  };
+
   return (
-    <header>
-      <div>HEADER</div>
+    <header className={css.header}>
+      <select
+        value={selectedChoice}
+        onChange={handleChange}
+        className={css.select}
+      >
+        <option value="" disabled>
+          Виберіть...
+        </option>
+        <option value="option1">Опція 1</option>
+        <option value="option2">Опція 2</option>
+        <option value="option3">Опція 3</option>
+      </select>
     </header>
   );
 };
