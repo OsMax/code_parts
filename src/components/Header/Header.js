11b1a122147ef0c476968698208f350f4d9516cd
OsMax <www.osmax@gmail.com>
2024-12-10 14:57:43 +0200

    some style for select

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,26 +7,26 @@ const Header = () => {
   const dispatch = useDispatch();
   const selectedChoice = useSelector((state) => state.choice.selectedChoice);
 
-  // const [choice, setCoice] = useState("");
-
   const handleChange = (event) => {
     dispatch(setChoice(event.target.value));
   };
 
   return (
     <header className={css.header}>
-      <select
-        value={selectedChoice}
-        onChange={handleChange}
-        className={css.select}
-      >
-        <option value="" disabled style={{ color: "#888" }}>
-          select an item...
-        </option>
-        <option value="option1">SingUp</option>
-        <option value="option2">Опція 2</option>
-        <option value="option3">Опція 3</option>
-      </select>
+      <div className={css.selectCont}>
+        <select
+          value={selectedChoice}
+          onChange={handleChange}
+          className={css.select}
+        >
+          <option value="" disabled style={{ color: "#888" }}>
+            select an item...
+          </option>
+          <option value="SingUp">SingUp</option>
+          <option value="List">List</option>
+          <option value="option3">Опція 3</option>
+        </select>
+      </div>
     </header>
   );
 };
