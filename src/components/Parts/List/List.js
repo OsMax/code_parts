c830a2142398d4f56bc3fe6a390a1e36dd247562
OsMax <www.osmax@gmail.com>
2024-12-16 15:02:21 +0200

    test prismJs

--- src/components/Parts/List/List.js
+++ src/components/Parts/List/List.js
@@ -1,5 +1,34 @@
+import React, { useEffect } from "react";
+import Prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.min.css";
+import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-css";
+
+const code = `
 const List = () => {
-  return <div>List...</div>;
+  return (
+    <div>
+      <pre>
+        <code className="language-javascript">{code}</code>
+      </pre>
+    </div>
+  );
+};
+`;
+
+const List = () => {
+  useEffect(() => {
+    Prism.highlightAll();
+  }, []);
+
+  return (
+    <div>
+      <h1>Пример подсветки кода</h1>
+      <pre>
+        <code className="language-javascript">{code}</code>
+      </pre>
+    </div>
+  );
 };
 
 export default List;
