bf379d76e390def8c7f75dccfb9123b1563fd3fe
OsMax <www.osmax@gmail.com>
2025-01-09 13:24:22 +0200

    Update SingUp.js

--- src/components/Parts/SingUp/SingUp.js
+++ src/components/Parts/SingUp/SingUp.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import css from "./SingUp.module.css";
 
 const SingUp = () => {
   const [formData, setFormData] = useState({
@@ -28,48 +29,15 @@ const SingUp = () => {
   };
 
   return (
-    <div
-      style={{
-        position: "relative",
-        margin: "auto",
-        marginTop: "52px",
-        maxWidth: "320px",
-        padding: "10px",
-        borderRight: " 2px solid #ccc",
-        borderBottom: "2px solid #ccc",
-        borderTop: "2px solid #888",
-        borderLeft: "2px solid #888",
-        // box-shadow: "2px 2px 2px #bbb";
-        borderEndEndRadius: "10px",
-        boxShadow: "2px 2px 2px #bbb",
-        // boxSizing: "border-box",
-      }}
-    >
-      <div
-        style={{
-          color: "#fff",
-          position: "absolute",
-          top: "-38px",
-          left: "-2px",
-          display: "flex",
-          flexDirection: "row",
-          gap: "2px",
-          boxSizing: "border-box",
-        }}
-      >
+    <div className={css.container}>
+      <div className={css.choiseConteiner}>
         <label
           name="log"
+          className={css.forChoise}
           style={{
-            border: "2px solid #888",
-            padding: "8px",
-            borderTopLeftRadius: "10px",
-            borderTopRightRadius: "10px",
             borderBottomColor: isLogin ? "#282c33" : "#888",
             textDecoration: isLogin ? "underline" : "none",
-            boxShadow: isLogin
-              ? "1px -1px 1px #bbb" // Левый верхний и правый
-              : "none",
-            cursor: "pointer",
+            boxShadow: isLogin ? "1px -1px 1px #bbb" : "none",
           }}
           onClick={changeIsLogin}
         >
@@ -77,17 +45,11 @@ const SingUp = () => {
         </label>
         <label
           name="reg"
+          className={css.forChoise}
           style={{
-            border: "2px solid #888",
-            padding: "8px",
-            borderTopLeftRadius: "10px",
-            borderTopRightRadius: "10px",
             borderBottomColor: !isLogin ? "#282c33" : "#888",
             textDecoration: !isLogin ? "underline" : "none",
-            boxShadow: !isLogin
-              ? "1px -1px 1px #bbb" // Левый верхний и правый
-              : "none",
-            cursor: "pointer",
+            boxShadow: !isLogin ? "1px -1px 1px #bbb" : "none",
           }}
           onClick={changeIsLogin}
         >
@@ -95,102 +57,46 @@ const SingUp = () => {
         </label>
       </div>
 
-      <form
-        style={{ width: "100%", marginTop: "10px", boxSizing: "border-box" }}
-      >
-        <div style={{ marginBottom: "15px" }}>
-          <label
-            htmlFor="email"
-            style={{ display: "block", marginBottom: "5px", color: "#ddd" }}
-          >
-            Email:
-          </label>
+      <form className={css.form}>
+        <label htmlFor="email" className={css.label}>
+          Email:
           <input
             type="email"
             id="email"
             name="email"
             value={formData.email}
             onChange={handleChange}
-            style={{
-              boxSizing: "border-box",
-              width: "100%",
-              padding: "10px",
-              border: "1px solid #ccc",
-              borderRadius: "4px",
-            }}
+            className={css.input}
           />
-          {errors.email && (
-            <p style={{ color: "red", fontSize: "14px" }}>{errors.email}</p>
-          )}
-        </div>
+        </label>
 
-        <div style={{ marginBottom: "15px" }}>
-          <label
-            htmlFor="password"
-            style={{ display: "block", marginBottom: "5px", color: "#ddd" }}
-          >
-            Password:
-          </label>
+        {errors.email && <p className={css.error}>{errors.email}</p>}
+        <label htmlFor="password" className={css.label}>
+          Password:{" "}
           <input
             type="password"
             id="password"
             name="password"
             value={formData.password}
             onChange={handleChange}
-            style={{
-              boxSizing: "border-box",
-              width: "100%",
-              padding: "10px",
-              border: "1px solid #ccc",
-              borderRadius: "4px",
-            }}
+            className={css.input}
           />
-          {!isLogin && (
-            <>
-              <label
-                htmlFor="password"
-                style={{
-                  display: "block",
-                  marginBottom: "5px",
-                  marginTop: "15px",
-                  color: "#ddd",
-                }}
-              >
-                Password:
-              </label>
-              <input
-                type="password"
-                id="password2"
-                name="password2"
-                value={formData.password2}
-                onChange={handleChange}
-                style={{
-                  boxSizing: "border-box",
-                  width: "100%",
-                  padding: "10px",
-                  border: "1px solid #ccc",
-                  borderRadius: "4px",
-                }}
-              />
-            </>
-          )}
-          {errors.password && (
-            <p style={{ color: "red", fontSize: "14px" }}>{errors.password}</p>
-          )}
-        </div>
-
-        <button
-          type="submit"
-          style={{
-            width: "100%",
-            padding: "10px",
-            background: "#007BFF",
-            color: "#fff",
-            border: "none",
-            borderRadius: "4px",
-            cursor: "pointer",
-          }}
-        >
+        </label>
+        {!isLogin && (
+          <label htmlFor="password" className={css.label}>
+            Password:
+            <input
+              type="password"
+              id="password2"
+              name="password2"
+              value={formData.password2}
+              onChange={handleChange}
+              className={css.input}
+            />
+          </label>
+        )}
+        {errors.password && <p className={css.error}>{errors.password}</p>}
+        <button type="submit" className={css.submitBtn}>
           {isLogin ? "Login" : "Registration"}
         </button>
       </form>
