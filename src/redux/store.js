26801507394895a99ce6057038693f9142141f1a
OsMax <www.osmax@gmail.com>
2025-01-14 16:36:54 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,22 +1,21 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+// import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-// Slice для управління станом опцій
-const choiceSlice = createSlice({
-  name: "choice",
-  initialState: { selectedChoice: "" },
-  reducers: {
-    setChoice: (state, action) => {
-      state.selectedChoice = action.payload;
-    },
-  },
-});
+// const choiceSlice = createSlice({
+//   name: "choice",
+//   initialState: { selectedChoice: "" },
+//   reducers: {
+//     setChoice: (state, action) => {
+//       state.selectedChoice = action.payload;
+//     },
+//   },
+// });
 
-export const { setChoice } = choiceSlice.actions;
+// export const { setChoice } = choiceSlice.actions;
 
-const store = configureStore({
-  reducer: {
-    choice: choiceSlice.reducer,
-  },
-});
+// const store = configureStore({
+//   reducer: {
+//     choice: choiceSlice.reducer,
+//   },
+// });
 
-export default store;
+// export default store;
