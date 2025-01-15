d392390075a7dd6f4faa2de0f3c42e9b1dcd6bb0
OsMax <www.osmax@gmail.com>
2025-01-15 16:14:37 +0200

    rebuild to page like

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import { Provider } from "react-redux";
 import "./App.css";
-import Header from "./components/Header/Header";
-import Main from "./components/Main/Main";
-import store from "./redux/store";
+import Layout from "./components/Layout/Layout";
+import Home from "./components/pages/Home/Home";
+
+import List from "./components/Parts/List/List";
+import SingUp from "./components/Parts/SingUp/SingUp";
+
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <Provider store={store}>
-      <Header />
-      <Main />
-    </Provider>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="list" element={<List />} />
+        <Route path="sing" element={<SingUp />} />
+        {/* <Route path="about" element={<AboutPage />} />
+          <Route path="contacts" element={<ContactPage />} /> */}
+      </Route>
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 }
 
