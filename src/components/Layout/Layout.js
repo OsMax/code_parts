54527dfa38c99f0de1fb1bffcdf1881146612757
OsMax <www.osmax@gmail.com>
2025-01-15 16:14:46 +0200

    Create Layout.js

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+};
+
+export default Layout;
