908cc2d106dbdc36951306932c62593023462266
OsMax <www.osmax@gmail.com>
2025-01-15 16:14:49 +0200

    Update Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,34 +1,12 @@
-import { useSelector } from "react-redux";
-import { useEffect, useState, lazy, Suspense } from "react";
-// import SingUp from "../Parts/SingUp/SingUp.js";
-
-const componentsMap = {
-  SingUp: lazy(() => import("../Parts/SingUp/SingUp.js")),
-  List: lazy(() => import("../Parts/List/List.js")),
-};
+import { Suspense } from "react";
+import { Outlet } from "react-router-dom";
 
 const Main = () => {
-  const selectedChoice = useSelector((state) => state.choice.selectedChoice);
-  const [ComponentDyn, setComponentDyn] = useState(
-    componentsMap[selectedChoice]
-  );
-
-  useEffect(() => {
-    if (selectedChoice) {
-      setComponentDyn(componentsMap[selectedChoice]);
-    }
-  }, [selectedChoice]);
-
   return (
     <main style={{ paddingTop: 48, paddingLeft: 20, paddingRight: 20 }}>
-      {!ComponentDyn && <div>Choice component for watching...</div>}
-      {ComponentDyn && (
-        <div>
-          <Suspense>
-            <ComponentDyn />
-          </Suspense>
-        </div>
-      )}
+      <Suspense>
+        <Outlet />
+      </Suspense>
     </main>
   );
 };
