a8be5031c39ab167817ed16eb2adb5e219853d27
OsMax <www.osmax@gmail.com>
2025-01-15 16:14:42 +0200

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,29 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { setChoice } from "../../redux/store";
+import { useNavigate } from "react-router-dom";
 import css from "./Header.module.css";
 
 const Header = () => {
-  const dispatch = useDispatch();
-  const selectedChoice = useSelector((state) => state.choice.selectedChoice);
+  const navigate = useNavigate();
 
   const handleChange = (event) => {
-    dispatch(setChoice(event.target.value));
+    console.log(event.target.value);
+
+    navigate(`/${event.target.value}`);
   };
 
   return (
     <header className={css.header}>
       <div className={css.selectCont}>
         <select
-          value={selectedChoice}
+          // value={"selectedChoice"}
           onChange={handleChange}
           className={css.select}
         >
-          <option value="" disabled style={{ color: "#888" }}>
+          <option disabled style={{ color: "#888" }}>
             select an item...
           </option>
-          <option value="SingUp">SingUp</option>
-          <option value="List">List</option>
+          <option value="list">List</option>
+          <option value="sing">SingUp</option>
           <option value="option3">Опція 3</option>
         </select>
       </div>
