f63fdc221fa9c411f799ee6ee58d35df35b4e702
OsMax <www.osmax@gmail.com>
2025-01-17 14:22:48 +0200

    css example string

---
+++ src/components/Parts/List/List.module.cssExample.js
@@ -0,0 +1,39 @@
+export const cssExample = `/*EXAMPLE CSS*/
+
+.container {
+  padding: 20px;
+}
+
+.container h1 {
+  color: #fff;
+  font-size: 20px;
+}
+.container h1 span {
+  color: rgb(199, 120, 221);
+}
+
+.listContainer {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+.list {
+  width: 280px;
+  border: 1px solid #fff;
+  border-radius: 5;
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  padding: 8px;
+  list-style: none;
+  color: #fff;
+}
+
+.listItem {
+  padding: 6px;
+  box-shadow: 2px 2px 2px #bbb;
+}
+
+`;
