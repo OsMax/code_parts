b0e9dda785c4cc8e8deaa3d676282904945490f2
OsMax <www.osmax@gmail.com>
2025-01-17 14:23:02 +0200

    Update List.js

--- src/components/Parts/List/List.js
+++ src/components/Parts/List/List.js
@@ -4,7 +4,9 @@ import "prismjs/themes/prism-tomorrow.min.css";
 import "prismjs/components/prism-javascript";
 import "prismjs/components/prism-css";
 import { example } from "./ListExample";
+import { cssExample } from "./List.module.cssExample";
 import axios from "axios";
+import css from "./List.module.css";
 
 // const array = ["example 1", "example 2", "example 3"];
 
@@ -32,47 +34,43 @@ const List = () => {
   }, []);
 
   return (
-    <div style={{ padding: 20 }}>
-      <h1 style={{ color: "#fff", fontSize: 20 }}>
-        <span style={{ color: "rgb(199, 120, 221)" }}>/</span>list
+    <div className={css.container}>
+      <h1>
+        <span>/</span>list
       </h1>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <ul
-          style={{
-            width: 280,
-            border: "1px solid #fff",
-            borderRadius: 5,
-            display: "flex",
-            flexDirection: "column",
-            gap: 12,
-            padding: 8,
-            listStyle: "none",
-            color: "#fff",
-          }}
-        >
+      <div className={css.listContainer}>
+        <ul className={css.list}>
           {list.length > 0 ? (
             list.map((item) => {
-              return (
-                <li style={{ padding: 6, boxShadow: "2px 2px 2px #bbb" }}>
-                  {item.item}
-                </li>
-              );
+              return <li className={css.listItem}>{item.item}</li>;
             })
           ) : (
             <p style={{ color: "#bbb" }}>No items available</p>
           )}
         </ul>
       </div>
-      <pre style={{ maxHeight: 200 }}>
-        <code className="language-javascript">{example}</code>
-      </pre>
+      <div style={{ display: "flex", gap: 10, flexWrap: "wrap" }}>
+        <pre
+          style={{
+            maxHeight: "200px",
+            width: "100%",
+            scrollbarWidth: "thin",
+            scrollbarColor: "#888 #2d2d2d",
+          }}
+        >
+          <code className="language-javascript">{example}</code>
+        </pre>
+        <pre
+          style={{
+            maxHeight: "200px",
+            width: "100%",
+            scrollbarWidth: "thin",
+            scrollbarColor: "#888 #2d2d2d",
+          }}
+        >
+          <code className="language-css">{cssExample}</code>
+        </pre>
+      </div>
     </div>
   );
 };
