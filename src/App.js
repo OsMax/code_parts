084bc1232cd5e76d336802750a992236a0e5552c
OsMax <www.osmax@gmail.com>
2025-01-28 15:02:06 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Layout from "./components/Layout/Layout";
 import Home from "./components/pages/Home/Home";
 
+import Loaders from "./components/Parts/Loaders/Loaders";
 import List from "./components/Parts/List/List";
 import AuthForm from "./components/Parts/AuthForm/AuthForm";
 
@@ -12,10 +13,9 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="loaders" element={<Loaders />} />
         <Route path="list" element={<List />} />
         <Route path="auth" element={<AuthForm />} />
-        {/* <Route path="about" element={<AboutPage />} />
-          <Route path="contacts" element={<ContactPage />} /> */}
       </Route>
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
