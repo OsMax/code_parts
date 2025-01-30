8302c0582bf4c483303f95609ac380ae16a94732
OsMax <www.osmax@gmail.com>
2025-01-30 16:25:32 +0200

    PersistGate  add

--- src/App.js
+++ src/App.js
@@ -7,18 +7,25 @@ import List from "./components/Parts/List/List";
 import AuthForm from "./components/Parts/AuthForm/AuthForm";
 
 import { Routes, Route, Navigate } from "react-router-dom";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="loaders" element={<Loaders />} />
-        <Route path="list" element={<List />} />
-        <Route path="auth" element={<AuthForm />} />
-      </Route>
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="loaders" element={<Loaders />} />
+            <Route path="list" element={<List />} />
+            <Route path="auth" element={<AuthForm />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </PersistGate>
+    </Provider>
   );
 }
 
