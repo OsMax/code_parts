6ed20e35ea9ca81fe3a78ee514a8b9a5ee097da4
OsMax <www.osmax@gmail.com>
2025-01-30 16:25:52 +0200

    Create listOperation.js

---
+++ src/redux/List/listOperation.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+// const BASEURL = "https://code-parts-server.onrender.com";
+
+// axios.defaults.baseURL = BASEURL;
+
+export const getList = createAsyncThunk("list/getList", async () => {
+  try {
+    const { data } = await axios.get(
+      "https://code-parts-server.onrender.com/api/list"
+    );
+    // console.log(data);
+
+    return { data };
+  } catch (error) {
+    console.log(error);
+  }
+});
