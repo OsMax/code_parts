fe27aa21049d4661c8a95d9d30647ef7236538cd
OsMax <www.osmax@gmail.com>
2025-01-31 12:22:05 +0200

    loaders like elements

---
+++ src/components/Parts/Loaders/Loader1/Loader1.js
@@ -0,0 +1,11 @@
+import css from "./Loader1.module.css";
+
+const Loader1 = () => {
+  return (
+    <div className={css.loaderont}>
+      <div className={css.loader}></div>
+    </div>
+  );
+};
+
+export default Loader1;
--- src/components/Parts/Loaders/Loaders.js
+++ src/components/Parts/Loaders/Loaders.js
@@ -1,12 +1,26 @@
+import Prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.min.css";
+import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-css";
+import React, { useEffect } from "react";
+
 import css from "./Loader.module.css";
+import Loader1 from "./Loader1/Loader1";
 
 const Loaders = () => {
+  useEffect(() => {
+    Prism.highlightAll();
+  }, []);
   return (
     <div className={css.container}>
       <h1>
         <span>/</span>loaders
       </h1>
-      <div className={css.loader}></div>
+      <ul>
+        <li>
+          <Loader1 />
+        </li>
+      </ul>
     </div>
   );
 };
