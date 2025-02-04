fa522ad0864ec113d8ca72a0cc5debe29cf42fcf
OsMax <www.osmax@gmail.com>
2025-02-04 14:01:23 +0200

    Update Loaders.js

--- src/components/Parts/Loaders/Loaders.js
+++ src/components/Parts/Loaders/Loaders.js
@@ -6,6 +6,7 @@ import React, { useEffect } from "react";
 
 import css from "./Loader.module.css";
 import Loader1 from "./Loader1/Loader1";
+import { loader1 } from "./loadersExamle";
 
 const Loaders = () => {
   useEffect(() => {
@@ -16,9 +17,40 @@ const Loaders = () => {
       <h1>
         <span>/</span>loaders
       </h1>
-      <ul>
-        <li>
-          <Loader1 />
+      <ul style={{ listStyleType: "none", paddingLeft: 0 }}>
+        <li
+          style={{
+            display: "flex",
+            width: "100%",
+            gap: "10px",
+            justifyContent: "space-around",
+            alignItems: "center",
+            flexWrap: "wrap",
+          }}
+        >
+          <div style={{ width: "30%" }}>
+            <Loader1 />
+          </div>
+          <pre
+            style={{
+              width: "30%",
+              height: "240px",
+              scrollbarWidth: "thin",
+              scrollbarColor: "#888 #2d2d2d",
+            }}
+          >
+            <code className="language-javascript">{loader1.js}</code>
+          </pre>
+          <pre
+            style={{
+              width: "30%",
+              height: "240px",
+              scrollbarWidth: "thin",
+              scrollbarColor: "#888 #2d2d2d",
+            }}
+          >
+            <code className="language-javascript">{loader1.css}</code>
+          </pre>
         </li>
       </ul>
     </div>
