aadfe4738430565b4bc78c7f03eae821486328af
OsMax <www.osmax@gmail.com>
2025-02-05 15:41:37 +0200

    Update loadersExamle.js

--- src/components/Parts/Loaders/loadersExamle.js
+++ src/components/Parts/Loaders/loadersExamle.js
@@ -42,3 +42,35 @@ export const loader1 = {
 }
 `,
 };
+
+export const loader2 = {
+  js: `
+<div className={css.loaderont}>
+    <div className={css.loader}></div>
+</div>
+`,
+  css: `
+.loader {
+  --d: 22px;
+  width: 4px;
+  height: 4px;
+  border-radius: 50%;
+  color: #25b09b;
+  box-shadow: 
+    calc(1 * var(--d))      calc(0 * var(--d))     0 0,
+    calc(0.707 * var(--d))  calc(0.707 * var(--d)) 0 1px,
+    calc(0 * var(--d))      calc(1 * var(--d))     0 2px,
+    calc(-0.707 * var(--d)) calc(0.707 * var(--d)) 0 3px,
+    calc(-1 * var(--d))     calc(0 * var(--d))     0 4px,
+    calc(-0.707 * var(--d)) calc(-0.707 * var(--d)) 0 5px,
+    calc(0 * var(--d))      calc(-1 * var(--d))    0 6px;
+  animation: l27 1s infinite linear;
+}
+
+@keyframes l27 {
+  100% {
+    transform: rotate(1turn);
+  }
+}
+`,
+};
