14e051d82e5e6dcdd66fd4841cd2e342350473c2
OsMax <www.osmax@gmail.com>
2025-02-05 15:40:48 +0200

    first loader rebuild

--- src/components/Parts/Loaders/Loader1/Loader1.js
+++ src/components/Parts/Loaders/Loader1/Loader1.js
@@ -1,9 +1,49 @@
+import React, { useEffect } from "react";
+
 import css from "./Loader1.module.css";
+import Prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.min.css";
+import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-css";
+
+import { loader1 } from "./loader1Examle";
 
 const Loader1 = () => {
+  useEffect(() => {
+    Prism.highlightAll();
+  }, []);
   return (
-    <div className={css.loaderont}>
-      <div className={css.loader}></div>
+    <div className={css.loadercont}>
+      <div
+        style={{
+          width: "15%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div className={css.loader}></div>
+      </div>
+      <pre
+        style={{
+          width: "30%",
+          height: "240px",
+          scrollbarWidth: "thin",
+          scrollbarColor: "#888 #2d2d2d",
+        }}
+      >
+        <code className="language-javascript">{loader1.js}</code>
+      </pre>
+      <pre
+        style={{
+          width: "30%",
+          height: "240px",
+          scrollbarWidth: "thin",
+          scrollbarColor: "#888 #2d2d2d",
+        }}
+      >
+        <code className="language-javascript">{loader1.css}</code>
+      </pre>
     </div>
   );
 };
