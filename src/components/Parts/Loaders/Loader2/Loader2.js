8f73240459808d93d5ab5aaa84e146091e0499ad
OsMax <www.osmax@gmail.com>
2025-02-07 10:50:37 +0200

    rebuild loader2

--- src/components/Parts/Loaders/Loader2/Loader2.js
+++ src/components/Parts/Loaders/Loader2/Loader2.js
@@ -1,17 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import css from "./Loader2.module.css";
-import Prism from "prismjs";
-import "prismjs/themes/prism-tomorrow.min.css";
-import "prismjs/components/prism-javascript";
-import "prismjs/components/prism-css";
 
-import { loader2 } from "./loader2Examle";
+import { loaderExample } from "./loader2Examle";
+import CodeWrite from "../CodeWrite";
 
 const Loader2 = () => {
-  useEffect(() => {
-    Prism.highlightAll();
-  }, []);
   return (
     <div className={css.loadercont}>
       <div
@@ -24,26 +18,7 @@ const Loader2 = () => {
       >
         <div className={css.loader}></div>
       </div>
-      <pre
-        style={{
-          width: "30%",
-          height: "240px",
-          scrollbarWidth: "thin",
-          scrollbarColor: "#888 #2d2d2d",
-        }}
-      >
-        <code className="language-javascript">{loader2.js}</code>
-      </pre>
-      <pre
-        style={{
-          width: "30%",
-          height: "240px",
-          scrollbarWidth: "thin",
-          scrollbarColor: "#888 #2d2d2d",
-        }}
-      >
-        <code className="language-javascript">{loader2.css}</code>
-      </pre>
+      <CodeWrite loaderExample={loaderExample} />
     </div>
   );
 };
