7ddbf715270d7500298f38e8211ae4543e176055
OsMax <www.osmax@gmail.com>
2025-02-17 13:18:36 +0200

    Update AuthForm.js

--- src/components/Parts/AuthForm/AuthForm.js
+++ src/components/Parts/AuthForm/AuthForm.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { logIn, register } from "../../../redux/Auth/authOperation";
+import { selectIsLogIn } from "../../../redux/Auth/authSelector";
 import css from "./AuthForm.module.css";
+import IsLogin from "./IsLogin/IsLogin";
 
 const SingUp = () => {
   const dispatch = useDispatch();
@@ -52,77 +54,79 @@ const SingUp = () => {
       <h1 className={css.title}>
         <span>/</span>authForm
       </h1>
-      <div className={css.container}>
-        <div className={css.choiseConteiner}>
-          <label
-            name="log"
-            className={css.forChoise}
-            style={{
-              borderBottomColor: checkForm ? "#282c33" : "#888",
-              textDecoration: checkForm ? "underline" : "none",
-              boxShadow: checkForm ? "1px -1px 1px #bbb" : "none",
-            }}
-            onClick={changeIsLogin}
-          >
-            Login
-          </label>
-          <label
-            name="reg"
-            className={css.forChoise}
-            style={{
-              borderBottomColor: !checkForm ? "#282c33" : "#888",
-              textDecoration: !checkForm ? "underline" : "none",
-              boxShadow: !checkForm ? "1px -1px 1px #bbb" : "none",
-            }}
-            onClick={changeIsLogin}
-          >
-            Registration
-          </label>
-        </div>
+      {!isLogin && (
+        <div className={css.container}>
+          <div className={css.choiseConteiner}>
+            <label
+              name="log"
+              className={css.forChoise}
+              style={{
+                borderBottomColor: checkForm ? "#282c33" : "#888",
+                textDecoration: checkForm ? "underline" : "none",
+                boxShadow: checkForm ? "1px -1px 1px #bbb" : "none",
+              }}
+              onClick={changeIsLogin}
+            >
+              Login
+            </label>
+            <label
+              name="reg"
+              className={css.forChoise}
+              style={{
+                borderBottomColor: !checkForm ? "#282c33" : "#888",
+                textDecoration: !checkForm ? "underline" : "none",
+                boxShadow: !checkForm ? "1px -1px 1px #bbb" : "none",
+              }}
+              onClick={changeIsLogin}
+            >
+              Registration
+            </label>
+          </div>
 
-        <form className={css.form} onSubmit={submit}>
-          <label htmlFor="email" className={css.label}>
-            Email:
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              className={css.input}
-            />
-          </label>
-
-          {/* {errors.email && <p className={css.error}>{errors.email}</p>} */}
-          <label htmlFor="password" className={css.label}>
-            Password:{" "}
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              className={css.input}
-            />
-          </label>
-          {!checkForm && (
-            <label htmlFor="password" className={css.label}>
-              Password:
+          <form className={css.form} onSubmit={submit}>
+            <label htmlFor="email" className={css.label}>
+              Email:
               <input
-                type="password"
-                id="password2"
-                name="password2"
-                value={formData.password2}
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
                 onChange={handleChange}
                 className={css.input}
               />
             </label>
-          )}
-          <button type="submit" className={css.submitBtn}>
-            {checkForm ? "Login" : "Registration"}
-          </button>
-        </form>
-      </div>
+
+            {/* {errors.email && <p className={css.error}>{errors.email}</p>} */}
+            <label htmlFor="password" className={css.label}>
+              Password:{" "}
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                className={css.input}
+              />
+            </label>
+            {!checkForm && (
+              <label htmlFor="password" className={css.label}>
+                Password:
+                <input
+                  type="password"
+                  id="password2"
+                  name="password2"
+                  value={formData.password2}
+                  onChange={handleChange}
+                  className={css.input}
+                />
+              </label>
+            )}
+            <button type="submit" className={css.submitBtn}>
+              {checkForm ? "Login" : "Registration"}
+            </button>
+          </form>
+        </div>
+      )}
     </>
   );
 };
