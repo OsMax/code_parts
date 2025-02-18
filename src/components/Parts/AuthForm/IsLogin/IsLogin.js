6cc821ef25eb5834a947a340c7cfb77b36691d78
OsMax <www.osmax@gmail.com>
2025-02-18 14:42:09 +0200

    Update IsLogin.js

--- src/components/Parts/AuthForm/IsLogin/IsLogin.js
+++ src/components/Parts/AuthForm/IsLogin/IsLogin.js
@@ -1,8 +1,18 @@
+import { logOut } from "../../../../redux/Auth/authOperation";
+import { useDispatch } from "react-redux";
+
 const IsLogin = () => {
+  const dispatch = useDispatch();
   return (
     <div>
       <h1>You are olredy LogIn!!!</h1>
-      <button>LogOut</button>
+      <button
+        onClick={() => {
+          dispatch(logOut());
+        }}
+      >
+        LogOut
+      </button>
     </div>
   );
 };
