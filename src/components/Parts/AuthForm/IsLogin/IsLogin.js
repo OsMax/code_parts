17d79b17743683cbf751f93f39f293442bf84290
OsMax <www.osmax@gmail.com>
2025-02-19 14:40:58 +0200

    Update IsLogin.js

--- src/components/Parts/AuthForm/IsLogin/IsLogin.js
+++ src/components/Parts/AuthForm/IsLogin/IsLogin.js
@@ -4,12 +4,31 @@ import { useDispatch } from "react-redux";
 const IsLogin = () => {
   const dispatch = useDispatch();
   return (
-    <div>
-      <h1>You are olredy LogIn!!!</h1>
+    <div
+      style={{
+        border: "1px solid #fff",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        maxWidth: "320px",
+        margin: "auto",
+      }}
+    >
+      <h1
+        style={{
+          color: "#fff",
+          margin: "0",
+          padding: "20px",
+          textAlign: "center",
+        }}
+      >
+        You are olredy LogIn!!!
+      </h1>
       <button
         onClick={() => {
           dispatch(logOut());
         }}
+        style={{ padding: "10px", fontSize: "16px", cursor: "pointer" }}
       >
         LogOut
       </button>
